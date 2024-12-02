075f97a6ce90b7a17fc797b50e0303a574063b3c
ImPhatDat <phatdatn2003@gmail.com>
2024-12-02 16:46:25 +0700

    rerun code

--- model/model.py
+++ model/model.py
@@ -3,9 +3,28 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class Layers(nn.Module):
+class SoundPro(nn.Module):
     def __init__(self):
-        super(Layers, self).__init__()
+        super(SoundPro, self).__init__()
+        
+    
+    def forward(self, x):
+        
+        return x
+
+class VideoPro(nn.Module):
+    def __init__(self):
+        super(VideoPro, self).__init__()
+        
+    
+    def forward(self, x):
+        
+        return x
+
+
+class Classifier(nn.Module):
+    def __init__(self):
+        super(Classifier, self).__init__()
         
     
     def forward(self, x):
